a5521513627f3699c32b2d446097d45ca1a2b966
Andrey Pravorskyi <apravorskii@electric-cloud.com>
2018-12-27 18:39:22 +0200

    Migrate to Gradle build system

--- src/main/resources/project/manifest.pl
+++
@@ -1,7 +0,0 @@
-@files = (
- ['//procedure[procedureName="runJasmine"]/propertySheet/property[propertyName="ec_parameterForm"]/value', 'runJasmineForm.xml'],
- ['//property[propertyName="scripts"]/propertySheet/property[propertyName="jasmineDriver"]/value','jasmineDriver.pl'],
- ['//property[propertyName="postp_matchers"]/value', 'postp_matchers.pl'],
- ['//property[propertyName="ec_setup"]/value', 'ec_setup.pl'],
- );
-
